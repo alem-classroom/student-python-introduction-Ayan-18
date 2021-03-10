9eb5923d73d21069d7bf172c6c991fb47d951372
Ayan-18 <80191082+Ayan-18@users.noreply.github.com>
2021-03-10 13:08:15 +0600

    Update conditional.py

--- if-else/conditional.py
+++ if-else/conditional.py
@@ -1,12 +1,17 @@
-num = int(input())
+import random
 
-if num > 0:
-    print(True)
-elif num % 2 == 0:
-    print(True)
-elif num > 0 and num % 2 == 0:
-    print(True)
-elif num > 0 or num % 2 == 0:
-    print(True)
-else:
-    print(False)
+def is_positive(num):
+    # return true if num is positive, otherwise return false
+    return num > 0
+def is_even(num):
+    # return true if num is even, otherwise return false
+    return num % 2 == 0
+
+def is_positive_and_even(num):
+    # return true if num is positive and even, otherwise return false
+    return num > 0 and num % 2 == 0
+
+def is_positive_or_even(num):
+    # return true if num is positive or even, otherwise return false
+    return num > 0 or num % 2 == 0
+
